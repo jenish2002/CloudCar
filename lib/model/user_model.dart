5ae7231b6f8fd8cd40cff6b1a0a4ffd2b63184e7
Jenish Patel <60685502+jenish2002@users.noreply.github.com>
2022-04-02 11:11:51 +0530

    Update user_model.dart

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -1,33 +1,3 @@
-// class UserModel {
-//   String? uid;
-//   String? email;
-//   String? firstName;
-//   String? secondName;
-//
-//   UserModel({this.uid, this.email, this.firstName, this.secondName});
-//
-//   //receive data from server
-//   factory UserModel.fromMap(map) {
-//     return UserModel(
-//       uid: map['uid'],
-//       email: map['email'],
-//       firstName: map['firstName'],
-//       secondName: map['secondName'],
-//     );
-//   }
-//
-//   //sending data to our server
-//    Map<String, dynamic> toMap() {
-//     return {
-//       'uid': uid,
-//       'email': email,
-//       'firstName': firstName,
-//       'secondName': secondName,
-//     };
-//    }
-// }
-
-
 import 'dart:convert';
 
 UserModel userModelFromJson(String str) => UserModel.fromJson(json.decode(str));
@@ -61,11 +31,3 @@ class UserModel {
     "uid": uid,
   };
 }
-
-
-
-
-
-
-
-
